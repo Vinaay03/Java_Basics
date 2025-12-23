a8871501800eaf9df6e43b2dac39913a0354cc67
Vinaay03 <iamvinay08@gmail.com>
2025-12-23 16:54:33 +0530

    product-sum

---
+++ productSum.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class productSum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int product = 1;
+        int sum = 0;
+
+        while(n>0){
+            int lastDigit = n%10;
+            product = product*lastDigit;
+            sum = sum+lastDigit;
+            n=n/10;
+        }
+        System.out.println(product-sum);
+        sc.close();
+    }
+}
